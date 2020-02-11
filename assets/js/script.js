ddcb65f854be92b10913586638b589434df60d60
j-pard <pardons.jonathan@gmail.com>
2020-02-11 19:47:29 +0100

    Fetch for featured movies & some corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 ( () => {
       const TOP_MOVIES = document.getElementById("top-movies");
+      const FEATURED_MOVIES = document.getElementById("featured-movies");
       const TEMPLATE = document.getElementById("template");
       
       const KEY = "7f7e0630f2410d5c2d9f0a18fc195d27";
-      let popularMovies = [];
+      let numberOfFeatured = 12;
 
       const createArticle = (target, source) => {
             let affiche = document.importNode(TEMPLATE.content, true);
@@ -15,13 +16,13 @@
             target.appendChild(affiche);
       }
 
-      const getPopular = async (url, number) => {
+      const getMovies = async (url, number, target) => {
             const RESPONSE = await fetch(url);
             const DATA = await RESPONSE.json();
             const RESULTS = await DATA.results;
-            const POPULAR_MOVIES = RESULTS.splice(0, number);
+            const RETURNED_MOVIES = RESULTS.splice(0, number);
             
-            POPULAR_MOVIES.forEach(async movie => {
+            RETURNED_MOVIES.forEach(async movie => {
                   const RESPONSE = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${KEY}`);
                   const DATA = await RESPONSE.json();
                   const VIDEO = await fetch(`http://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${KEY}`);
@@ -35,12 +36,26 @@
                         release: DATA.release_date.split("-").splice(0, 1),
                         trailer: TRAILER
                   }
-                  popularMovies.push(MOVIE);
-                  createArticle(TOP_MOVIES, MOVIE);
+                  createArticle(target, MOVIE);
             });
-            console.log(popularMovies);
       }
 
 
-      getPopular(`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}`, 5);
+      getMovies(`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}`, 5, TOP_MOVIES);
+      getMovies(`https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`, numberOfFeatured, FEATURED_MOVIES);
+
+      document.getElementById("btn-load-more").addEventListener("click", () => {
+            FEATURED_MOVIES.innerHTML = "";
+            if(numberOfFeatured == 12) {
+                  numberOfFeatured = 18;
+                  document.getElementById("btn-load-more").textContent = "Load Less";
+                  getMovies(`https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`, numberOfFeatured, FEATURED_MOVIES);
+            }
+            else {
+                  numberOfFeatured = 12;
+                  document.getElementById("btn-load-more").textContent = "Load More";
+                  getMovies(`https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`, numberOfFeatured, FEATURED_MOVIES);
+            }
+            
+      });
 })();
