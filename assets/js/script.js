6786ef4dd972df70adfe8141c061396025191086
Colart Lloyd <colart.lloyd@gmail.com>
2020-02-12 13:31:22 +0100

    contact no safe

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,4 +74,20 @@
                   NAVIGATOR.classList.remove('scroll');
             }
       })
+
+
+      // CONTACT US CONFIRM
+
+      document.getElementById('submitContact').addEventListener('click', ()=> {
+            let firstNameUser = document.getElementById('firstNameContact').value;
+            let lastNameUser = document.getElementById('lastNameContact').value;
+            let emailUser = document.getElementById('emailContact').value;
+            let subjectUser = document.getElementById('subjectContact').value;
+            let commentUser = document.getElementById('commentContact').value;
+            document.getElementById('firstNameContactConfirm').textContent = firstNameUser;
+            document.getElementById('lastNameContactConfirm').textContent = lastNameUser;
+            document.getElementById('emailContactConfirm').textContent = emailUser;
+            document.getElementById('subjectContactConfirm').textContent = subjectUser;
+            document.getElementById('commentContactConfirm').textContent = commentUser;
+      })
 })();
